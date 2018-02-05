51b72d6c3efd30f3c5df2001f2e828edccfbc4ac
madbaddog <36157066+madbaddog@users.noreply.github.com>
2018-02-05 15:51:15 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 console.log("start");
-console.log(document.getElementById("outcanvas"));
-console.log(document.getElementById("buttonSwitch"));
-var decoder = new _qrdecoder('outcanvas', 240, 180);
-decoder.start()
-.then(function(data){
-    console.log(data);
-});   
-function switchCam(){
-    decoder.switchCamera();
-}
+document.addEventListener("DOMContentLoaded", function(){
+    var decoder = new _qrdecoder('outcanvas', 240, 180);
+    decoder.start()
+    .then(function(data){
+        console.log(data);
+    });   
+    function switchCam(){
+        decoder.switchCamera();
+    }    
+});
+
