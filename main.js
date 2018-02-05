2643b8a1506fb2c8a5528897ec2ff59d53dd8d2b
madbaddog <36157066+madbaddog@users.noreply.github.com>
2018-02-05 16:30:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
-console.log("start");
 document.addEventListener("DOMContentLoaded", function(){
-    var decoder = new _qrdecoder('outcanvas', 240, 180);
-    decoder.start()
-    .then(function(data){
-        console.log(data);
-    });   
+//     var decoder = new _qrdecoder('outcanvas', 240, 180);
+//     decoder.start()
+//     .then(function(data){
+//         console.log(data);
+//     });   
     function switchCam(){
         decoder.switchCamera();
+            video = document.getElementById('video');
+            video.style.width = document.width + 'px';
+            video.style.height = document.height + 'px';
+            video.setAttribute('autoplay', '');
+            video.setAttribute('muted', '');
+            video.setAttribute('playsinline', '');
+
+            var constraints = {
+                 audio: false,
+                 video: {
+                     facingMode: 'user'
+                 }
+            }
+
+            navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+                video.srcObject = stream;
+            });
     } 
 });
 
