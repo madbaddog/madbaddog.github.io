900cdc1b6b47e556a9f2639dcdf0171fcac0c743
madbaddog <36157066+madbaddog@users.noreply.github.com>
2018-02-05 15:36:06 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+var decoder = new _getqrdecoder('outcanvas', 240, 180);
+decoder.start();
+.then(function(data){
+    console.log(data);
+});   
+function switchCam(){
+    decoder.switchCamera();
+}
