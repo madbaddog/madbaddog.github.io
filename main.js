191292dc3d4af3aaf8ec6f209b9b0468ef8bb804
madbaddog <36157066+madbaddog@users.noreply.github.com>
2018-02-05 15:48:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 console.log("start");
-console.log(document.getElementById('outcanvas'));
+console.log(document.getElementById("outcanvas"));
+console.log(document.getElementById("buttonSwitch"));
 var decoder = new _qrdecoder('outcanvas', 240, 180);
 decoder.start()
 .then(function(data){
