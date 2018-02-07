998e84c44d337f3e592437fc76f2705d36f3a5b3
madbaddog <36157066+madbaddog@users.noreply.github.com>
2018-02-07 10:48:17 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-document.addEventListener("load", function(){
+window.onload(function(){
     alert("page loaded");
     var decoder = new _qrdecoder('outcanvas', 240, 180);
     decoder.start()
     .then(function(data){
         console.log(data);
     });   
-//     document.getElementById("button").onclick = function(){
-//         decoder.switch();
-//         alert("switch cam");
-//     }
+    document.getElementById("button").onclick = function(){
+        decoder.switch();
+        alert("switch cam");
+    }
 });
