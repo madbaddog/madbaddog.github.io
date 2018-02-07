a954ee53e241ea552ae486ef7ad594018015913c
madbaddog <36157066+madbaddog@users.noreply.github.com>
2018-02-07 12:15:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-window.onload = function(){
+window.onload = function(){    
     var decoder = new _qrdecoder('outcanvas', 240, 180);
+    if (decoder.checkConstraints())
     decoder.start()
     .then(function(data){
         console.log(data);
     });   
-    document.getElementById("button").onclick = function(){
-        decoder.switchCamera();
-    }
 };
